409b76aa188bb8472b9edf00a7f3891afcca4531
ahmet kizilay <ahmet.kizilay@gmail.com>
2014-04-26 10:40:51 +0300

    implemented feed.html
    displaying users feed on the page. the basic template for each image
    is constructed.

--- pages/js/feeds.js
+++ pages/js/feeds.js
@@ -1,3 +1,113 @@
+var loginPage = (function (d) {
+    var _fn_convertTimestamp = function (timestamp) {
+        var yearInMS = 31557600000;
+        var weekInMS = 604800000;
+        var dayInMS = 86400000;
+        var hourInMS = 3600000;
+        var minInMS = 60000;
+        var secondInMS = 1000;
+
+        var span = new Date().getTime() - new Date(timestamp * 1000).getTime();
+
+        if(span > yearInMS) {
+            return Math.floor(span / yearInMS) + 'y';
+        }
+
+        if(span > weekInMS) {
+            return Math.floor(span / weekInMS) + 'w';
+        }
+
+        if(span > dayInMS) {
+            return Math.floor(span / dayInMS) + 'd';
+        }
+
+        if(span > hourInMS) {
+            return Math.floor(span / hourInMS) + 'h';
+        }
+
+        if(span > minInMS) {
+            return Math.floor(span / minInMS) + 'm';
+        }
+
+        return Math.floor(span / secondInMS) + 's';
+    };
+
+    // http://jsfiddle.net/g9hAx/2/
+    var _fn_constructImage = function (parent, imgData) {
+        console.log('start constr');
+
+        var postDiv = document.createElement('div');
+        postDiv.className = 'post';
+
+        // start top div
+        var topDiv = document.createElement('div');
+        topDiv.className = 'top';
+        var aUser = document.createElement('a');
+        aUser.innerHTML = imgData.user.username;
+        aUser.setAttribute('href', '#');
+        topDiv.appendChild(aUser);
+        // end top div
+
+        // start middle div
+        var middleDiv = document.createElement('div');
+        middleDiv.className = 'middle';
+        
+        var aImage = document.createElement('a');
+        aImage.setAttribute('href', '#');
+        var img = document.createElement('img');
+        img.setAttribute('src', imgData.images.thumbnail.url);
+        aImage.appendChild(img);
+        middleDiv.appendChild(aImage);
+        // end middle div
+
+        // start bottom div
+        var bottomDiv = document.createElement('div');
+        bottomDiv.className = 'bottom';
+        
+        var imgLike = document.createElement('img');
+        imgLike.setAttribute('src', 'img/heart.png');
+        bottomDiv.appendChild(imgLike);
+        
+        var lblLikeCount = document.createElement('label');
+        lblLikeCount.innerHTML = imgData.likes.count;
+        bottomDiv.appendChild(lblLikeCount);
+        
+        var imgComment = document.createElement('img');
+        imgComment.setAttribute('src', 'img/comment.png');
+        bottomDiv.appendChild(imgComment);
+
+        var lblCommentCount = document.createElement('label');
+        lblCommentCount.innerHTML = imgData.comments.count;
+        bottomDiv.appendChild(lblCommentCount);
+
+        var lblDate = document.createElement('label');
+        lblDate.innerHTML = _fn_convertTimestamp(imgData.created_time);
+        bottomDiv.appendChild(lblDate);
+        // end bottom div
+
+        postDiv.appendChild(topDiv);
+        postDiv.appendChild(middleDiv);
+        postDiv.appendChild(bottomDiv);
+
+        postDiv.addEventListener('mouseover', function () {
+            bottomDiv.className += " appear-with-ease";
+            topDiv.className += " appear-with-ease";
+        }, false);
+
+        postDiv.addEventListener('mouseout', function () {
+            bottomDiv.className = bottomDiv.className.replace(" appear-with-ease", "");
+            topDiv.className = topDiv.className.replace(" appear-with-ease", "");
+        }, false);
+
+        parent.appendChild(postDiv);
+
+    };
+
+    return {
+        constructImage: _fn_constructImage
+    };
+})(document);
+
 document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.local.get('user_data', function (res) {
         var user_data = res.user_data;
@@ -27,15 +137,7 @@ document.addEventListener('DOMContentLoaded', function() {
             var divFeed = document.getElementsByClassName('feed')[0];
             
             for(var i = 0; i < data.length; i += 1) {
-                var a = document.createElement('a');
-                var img = document.createElement('img');
-                img.src = data[i].images.thumbnail.url;
-                img.className = 'feed';
-
-                a.setAttribute('href', data[i].link);
-                a.appendChild(img);
-
-                divFeed.appendChild(a);
+                loginPage.constructImage(divFeed, data[i]);
             }
         });
     });
